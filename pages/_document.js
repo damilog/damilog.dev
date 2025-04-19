04b9e7dbfd0a0c377f1173daf7b1ba06c3bf48f6
damilog <dami.k.dev@gmail.com>
2025-04-19 17:29:30 +0900

    feat: SEO 인증 meta 태그 추가

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,10 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          <meta
+            name="google-site-verification"
+            content="_bmvxUpf2zeZYRZ77BTGeGpF-kqmaMz05OJCE0FrcsA"
+          />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
